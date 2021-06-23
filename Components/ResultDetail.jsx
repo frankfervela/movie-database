05eb93f5c537207061cdcadd176ca37d86a12d60
Frank Fernandez <Frafernandez@softekexport.com>
2021-06-22 23:33:11 -0400

    restored project to previous version

--- Components/ResultDetail.jsx
+++ Components/ResultDetail.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import Image from "next/image";
-import {useRouter} from "next/router";
 
 
 function ResultDetail({ movieResult }) {
-
-    const router = useRouter();
 
     const BASE_URL = "https://image.tmdb.org/t/p/original";
 
@@ -14,10 +11,9 @@ function ResultDetail({ movieResult }) {
             <Image className={"object-cover rounded-xl"} height={1080} width={1920}
                    src={`${BASE_URL}${movieResult.backdrop_path || movieResult.poster_path}` || `${BASE_URL}${movieResult.poster_path}`}/>
             <div className={"flex flex-col m-6"}>
-                <h3 className="font-bold mb-3 text-2xl md:text-3xl lg:text-5xl text-gray-200 movie--title">{movieResult.title}</h3>
-                <h4 className="movie--year text-xl lg:text-sm lg:mb-4">{movieResult.release_date || movieResult.first_air_date}</h4>
+                <h3 className="font-bold mb-3 text-xl md:text-3xl lg:text-5xl text-gray-200 movie--title">{movieResult.title} ({movieResult.release_date.substr(0,4) || movieResult.first_air_date})</h3>
                 <div className={"flex flex-row m-1"}>
-                    <p className={"inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-blue-500 rounded-full"}>Lenght: {movieResult.runtime}m</p>
+                    <p className={"inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-blue-500 rounded-full"}>Length: {movieResult.runtime}m</p>
                     <p className={"inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-green-600 rounded-full"}>Rating: {movieResult.vote_average}</p>
                 </div>
                 <div className="flex-grow mt-5">
