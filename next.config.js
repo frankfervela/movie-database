a652f46624a9f1e94ded9540d8164f1a25d2b445
Frank Fernandez <Frafernandez@softekexport.com>
2021-06-22 21:44:15 -0400

    Added corrections to ui and movie details page.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  exportTrailingSlash: true,
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    };
+  },
   images: {
     domains: ["hulu-matchmaker.s3.us-west-2.amazonaws.com", "image.tmdb.org"]
   }
